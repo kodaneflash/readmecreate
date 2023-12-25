c96d39540673afda66e6809bd135a059a908ac79
jamesonarcher <jamesonmarcher@gmail.com>
2023-12-25 16:30:51 -0500

    added vercel analytics  pages/_app.js added vercel analytics   pages/_document.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,13 @@ import { appWithTranslation } from "next-i18next";
 import NextNProgress from "nextjs-progressbar";
 import { green } from "tailwindcss/colors.js";
 
+// Import Vercel Analytics script (if available as a module)
+// import VercelAnalytics from 'path-to-vercel-analytics';
+
 const App = ({ Component, pageProps }) => {
+  // Initialize Vercel Analytics (if needed)
+  // VercelAnalytics.init();
+
   return (
     <>
       <NextNProgress color={green[500]} height={4} />
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Insert Vercel Analytics script tag here */}
+        {/* Example: <script src="path-to-vercel-analytics.js"></script> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }

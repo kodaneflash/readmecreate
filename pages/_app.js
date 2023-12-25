09cfb876c42eea6b766aad5591ac48fe6d842bc7
jamesonarcher <jamesonmarcher@gmail.com>
2023-12-25 16:39:58 -0500

    vercel analytics fix

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,14 @@ import "@/styles/globals.css";
 import { appWithTranslation } from "next-i18next";
 import NextNProgress from "nextjs-progressbar";
 import { green } from "tailwindcss/colors.js";
-
-// Import Vercel Analytics script (if available as a module)
-// import VercelAnalytics from 'path-to-vercel-analytics';
+import { Analytics } from '@vercel/analytics/react'; // Import the Analytics component
 
 const App = ({ Component, pageProps }) => {
-  // Initialize Vercel Analytics (if needed)
-  // VercelAnalytics.init();
-
   return (
     <>
       <NextNProgress color={green[500]} height={4} />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
+      <Analytics /> {/* Add the Analytics component */}
     </>
   );
 };
--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Insert Vercel Analytics script tag here */}
-        {/* Example: <script src="path-to-vercel-analytics.js"></script> */}
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
